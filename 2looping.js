04b17394c90bdf5f803b5a6fe0715d4a4be4195d
jakariawb <166798315+jakariawb@users.noreply.github.com>
2025-02-02 09:45:23 +0600

    first commit

---
+++ 2looping.js
@@ -0,0 +1,23 @@
+for(var looop = 0; looop <= 10; looop = looop + 1 ){
+
+    function nur (x , y){
+        var pay = x / y;
+        console.log('rusult = ' + pay)
+    }  
+    nur(3, 2)
+}
+console.log('END');
+
+var lop = 1;
+while(lop <= 10){
+
+    function maines (t, u){  
+        var n  = t - u;
+        console.log('rusult = ' + n)
+    }
+
+   lop = lop + lop;
+    maines(3,10)
+
+}
+console.log('END');
