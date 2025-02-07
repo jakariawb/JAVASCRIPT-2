fbcf4ee1de79f50535c1553fc8c0c11fc44da5e4
jakariawb <166798315+jakariawb@users.noreply.github.com>
2025-02-07 12:13:35 +0600

    first commit

---
+++ loop-for-aray.js
@@ -0,0 +1,19 @@
+
+// var num = [10,20,30,40]
+var num = new Array()
+
+for (var i=0; i<4; i++){
+     num[i] = parseInt(prompt('enter your first number'));
+
+}
+var sum = 0;
+
+for(var i = 0;i<4; i++){
+    console.log("uager Number ="+num[i]);
+    10
+    sum = sum + num[i];
+}
+console.log('sum number ='+ sum)
+
+
+
